38f663d14e480489d845e221fed60a2b5b155813
sovitchaudhary <sovitchaudhary45@gmail.com>
2025-03-22 03:40:01 +0530

    Add Header component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,15 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          {/* header section */}
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          {/* footer section */}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-300">
+              <p>developed by SMJ Group</p>
+            </div>
+          </footer>
         </ThemeProvider>
       </body>
     </html>
